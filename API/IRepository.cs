828422857f9cadd2752f9f8f3d88a5d314ce02f1
marco-soria <marco.soriaa7@gmail.com>
2025-08-21 14:41:39 -0500

    introductory backend

---
+++ API/IRepository.cs
@@ -0,0 +1,11 @@
+using API.Entities;
+
+namespace API;
+
+ public interface IRepository
+    {
+        List<Genre> GetAllGenres();
+        Task<Genre?> GetById(int id);
+        bool Exists(string name);
+        int Create(Genre genre);
+    }
--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,5 @@
+using API;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,15 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddOutputCache(options =>
+{
+    options.DefaultExpirationTimeSpan = TimeSpan.FromSeconds(60);
+});
+builder.Services.AddSingleton<IRepository, InMemoryRepository>();
+
+builder.Services.AddTransient<TransientService>();
+builder.Services.AddScoped<ScopedService>();
+builder.Services.AddSingleton<SingletonService>();
 
 var app = builder.Build();
 
@@ -15,6 +26,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseOutputCache();
 
 app.UseHttpsRedirection();
 
